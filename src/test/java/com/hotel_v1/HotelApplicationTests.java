9c873c1d470708f4bb7bfdb53d7fdb5be45e5966
carlo <carloszumbadocardenas@gmail.com>
2023-02-08 22:48:48 -0600

    Practica#2

---
+++ src/test/java/com/hotel_v1/HotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
